7d81a2be32418efbe94b6c9dcc93430973f8f2cf
David Shuster <d.shuster@visionix.com>
2026-01-02 14:27:18 +0200

    chore: Bump version to 1.7.1

--- src/music-assistant-playlist-card.ts
+++ src/music-assistant-playlist-card.ts
@@ -23,7 +23,7 @@ import type {
 import { TABS } from './types';
 
 // Card information for HACS
-const CARD_VERSION = '1.7.0';
+const CARD_VERSION = '1.7.1';
 
 // Log card info on load
 console.info(
